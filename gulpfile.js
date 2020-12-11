b98e60b97ec2d558a31ff2970fed4daa50c3423b
maxim <maximum454@gmail.com>
2020-12-11 11:23:57 +0600

    news-main.scss

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,6 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
-        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -122,11 +121,6 @@ function js() {
 
 function images() {
     return src(path.src.img)
-        .pipe(
-            webp({
-                quality: 70
-            })
-        )
         .pipe(dest(path.build.img))
         .pipe(src(path.src.img))
         .pipe(
--- src/js/mprshop.js
+++ src/js/mprshop.js
@@ -1,5 +1,4 @@
 @@include('./partials/jquery.min.js')
-@@include('./partials/webp.js')
 @@include('./partials/burger.js')
 @@include('./partials/slick.js')
 @@include('./partials/remodal.js')
