32c86f5e34dd9e10394e53625b28549099a7f602
maxim <maximum454@gmail.com>
2020-12-14 01:28:15 +0600

    accompany.scss

--- src/js/mprshop.js
+++ src/js/mprshop.js
@@ -7,5 +7,37 @@
 
 
 $(function () {
-
+    $('.js-slider-accompany').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: $('.accompany__next'),
+        prevArrow: $('.accompany__prev'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 4,
+                    arrows: false
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 })
