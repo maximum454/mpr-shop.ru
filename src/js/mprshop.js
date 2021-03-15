8d77b9f1cf6077edc8dac034b143b325b7106a49
maxim <maximum454@gmail.com>
2021-03-15 12:28:11 +0600

    body lock

--- src/js/mprshop.js
+++ src/js/mprshop.js
@@ -20,9 +20,11 @@
 $(function () {
     $('.header__burger').on('click', function (){
         $('.menu-mobile').addClass('active');
+        $('body').addClass('lock');
     })
     $('.js-mobile-close').on('click', function (){
         $('.menu-mobile').removeClass('active');
+        $('body').removeClass('lock');
     })
 
     $('.menu-catalog__list-li').on('click', function (){
