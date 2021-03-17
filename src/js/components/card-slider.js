84bc387acb3a11e9229e6b994e7ccc0b518ea57c
maximum <454>
2021-03-18 01:34:24 +0600

    articles-detail.html

--- src/js/components/card-slider.js
+++ src/js/components/card-slider.js
@@ -6,18 +6,27 @@ $(function () {
         speed: 300,
         autoplay: false,
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 4,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
+                    slidesToScroll: 3,
                 }
             },
             {
                 breakpoint: 767,
                 settings: {
-                    slidesToShow: 4,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
         ]
